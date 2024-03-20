def072873a6eca8f15b08f1d22bfd4783d7b956e
Riversity <maayihjia@sjtu.edu.cn>
2024-03-20 15:16:36 +0800

    Priority queue update.

--- priority_queue/src/priority_queue.hpp
+++ priority_queue/src/priority_queue.hpp
@@ -6,69 +6,143 @@
 #include "exceptions.hpp"
 
 namespace sjtu {
-
-/**
- * a container like std::priority_queue which is a heap internal.
- */
 template<typename T, class Compare = std::less<T>>
 class priority_queue {
+private:
+  class node {
+  public:
+    node* left;
+    node* right;
+    T* content;
+    int depth;
+    node() = delete;
+    node(const T &con, int d = 0) : depth(d) {
+      content = new T(con);
+      left = nullptr;
+      right = nullptr;
+    }
+    ~node() {
+      delete content;
+    }
+  };
+
+  int siz;
+  node* root;
+  //static Compare cmp;
+
 public:
-	/**
-	 * TODO constructors
-	 */
-	priority_queue() {}
-	priority_queue(const priority_queue &other) {}
-	/**
-	 * TODO deconstructor
-	 */
-	~priority_queue() {}
-	/**
-	 * TODO Assignment operator
-	 */
-	priority_queue &operator=(const priority_queue &other) {}
-	/**
-	 * get the top of the queue.
-	 * @return a reference of the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	const T & top() const {
+  priority_queue() {
+    siz = 0;
+    root = nullptr;
+  }
 
-	}
-	/**
-	 * TODO
-	 * push new element to the priority queue.
-	 */
-	void push(const T &e) {
+  priority_queue(const priority_queue &other) {
+    root = copy(other.root);
+    siz = other.siz;
+  }
 
-	}
-	/**
-	 * TODO
-	 * delete the top element.
-	 * throw container_is_empty if empty() returns true;
-	 */
-	void pop() {
+  ~priority_queue() {
+    destroy(root);
+  }
 
-	}
-	/**
-	 * return the number of the elements.
-	 */
-	size_t size() const {
+  priority_queue &operator=(const priority_queue &other) {
+    if(this == &other) return *this;
+    destroy(root);
+    root = copy(other.root);
+    siz = other.siz;
+    return *this;
+  }
+  /**
+   * get the top of the queue.
+   * @return a reference of the top element.
+   * throw container_is_empty if empty() returns true;
+   */
+  const T & top() const {
+    if(empty()) throw container_is_empty();
+    return *(root->content);
+  }
 
-	}
-	/**
-	 * check if the container has at least an element.
-	 * @return true if it is empty, false if it has at least an element.
-	 */
-	bool empty() const {
+  void push(const T &e) {
+    //cmp(*root->content, e);
+    node *add = new node(e);
+    try {
+      root = fuse(root, add);
+    }
+    catch(...) {
+      delete add;
+      throw;
+    }
+    ++siz;
+  }
 
-	}
-	/**
-	 * merge two priority_queues with at most O(logn) complexity.
-	 * clear the other priority_queue.
-	 */
-	void merge(priority_queue &other) {
+  void pop() {
+    if(empty()) throw container_is_empty();
+    --siz;
+    node* temp = root;
+    root = fuse(root->left, root->right);
+    delete temp;
+  }
 
-	}
+  size_t size() const {
+    return siz;
+  }
+
+  bool empty() const {
+    if(root) return false;
+    return true;
+  }
+  /**
+   * merge two priority_queues with at most O(logn) complexity.
+   * clear the other priority_queue.
+   */
+  void merge(priority_queue &other) {
+    root = fuse(root, other.root);
+    siz += other.siz;
+    other.siz = 0;
+    other.root = nullptr;
+  }
+
+private:
+  int dep(node* p) const {
+    if(p) return p->depth;
+    return -1;
+  }
+  void exchange(node* &x, node* &y) {
+    node* temp = x;
+    x = y;
+    y = temp;
+  }
+  node* copy(node* other) {
+    if(!other) return nullptr;
+    node* cur = new node(*(other->content), other->depth);
+    cur->left = copy(other->left);
+    cur->right = copy(other->right);
+    return cur;
+  }
+  void destroy(node* p) {
+    if(!p) return;
+    destroy(p->left);
+    destroy(p->right);
+    delete p;
+    return;
+  }
+  /*
+    fuse invalidate original relations.
+  */
+  node* fuse(node* x, node* y) {
+    if(!x) return y;
+    if(!y) return x;
+    if(Compare()(*(x->content), *(y->content))) {
+      exchange(x, y);
+    }
+    // x is the new root
+    x->right = fuse(x->right, y);
+    if(dep(x->left) < dep(x->right)) {
+      exchange(x->left, x->right);
+    }
+    x->depth = dep(x->right) + 1;
+    return x;
+  }
 };
 
 }
